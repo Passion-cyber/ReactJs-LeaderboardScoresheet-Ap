fbd8d1123214656c0f0548b13811f877c9fb2987
Passion-cyber <chineduibelachi@gmail.com>
2023-09-05 22:07:54 +0100

    Fix: First Commit

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import './navbar.css'
-
+import React from "react";
+import "./navbar.css";
 
 const NavBar = () => {
-   
   return (
-    <div className='nav-container'>
-    <header className="main-header">        
-      <div className="logo">
-        <a href="#">
-            <img src="./images/snap-shot.png" className='image-logo' alt="learnable-logo" />
-        </a>
-
-      </div>
-      <div className="navigation">
-        <button className='navigation-button'>Everything is learnable</button>
-      </div>
-    </header>
+    <div className="nav-container">
+      <header className="main-header">
+        <div className="logo">
+          <a href="https://www.genesystechhub.com/learnable">
+            <img
+              src={`https://res.cloudinary.com/dysznjd1z/image/upload/v1693946807/Leaderboard/snap-shot_hfh8mr.png`}
+              className="image-logo"
+              alt="learnable-logo"
+            />
+          </a>
+        </div>
+        <div className="navigation">
+          <button className="navigation-button">Everything is learnable</button>
+        </div>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
